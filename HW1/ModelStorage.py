0ff3ccb90e12503ff569c96b488f57f844ecff33
gerendenis <dgeren@bk.ru>
2022-10-23 13:45:50 +0300

    small change with load_models

--- HW1/ModelStorage.py
+++ HW1/ModelStorage.py
@@ -173,12 +173,12 @@ class ModelStorage(object):
 
             all_models = [f for f in os.listdir(self.save_path) if
                           os.path.isfile(os.path.join(self.save_path, f))]
-
+            print(all_models)
             models_info = {}
 
-            for model_id in all_models:
-                model_path = f'{self.save_path}/model_{model_id}'
-
+            for file in all_models:
+                model_path = f'{self.save_path}/{file}'
+                print(model_path)
                 with open(model_path, 'rb') as f:
                     model = pickle.load(f)
 
@@ -188,7 +188,8 @@ class ModelStorage(object):
 
                     if not comment:
                         comment = '-'
-
+                    
+                    model_id = int(str(file)[6:])
                     models_info[model_id] = f'Model_type: {model_type}, is_fitted : {is_fitted}, comment : {comment}'
 
             return models_info
